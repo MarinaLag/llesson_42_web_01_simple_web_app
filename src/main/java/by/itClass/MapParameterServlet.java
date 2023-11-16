c2e928f415dd2c9566db9ccf25ab9055791a8e55
Marina Lagoikina <marinalagoikina@gmail.com>
2023-11-16 21:50:58 +0300

    Initial commit

---
+++ src/main/java/by/itClass/MapParameterServlet.java
@@ -0,0 +1,27 @@
+package by.itClass;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Map;
+
+@WebServlet(name = "mapParameterServlet", urlPatterns = "/mapParam")
+public class MapParameterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        PrintWriter writer = resp.getWriter();
+        parameterMap.forEach((k, v) -> writer.write("<h1>" + k + " " + Arrays.toString(v)+ "</h1>"));
+
+        // запрос в браузере:
+        // http://localhost:8080/mapParam?parame=Hello&parame=Web&f=aaaaaa&d=bbbbb
+
+
+    }
+}
---
+++ src/main/java/by/itClass/OneParameterServlet.java
@@ -0,0 +1,26 @@
+package by.itClass;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "oneParameterServlet", urlPatterns = "/oneParam") // говорим веб сервису на который мы отправляем
+// oneParameterServlet -имя  /oneParam - патерн
+public class OneParameterServlet extends HttpServlet {
+    // отправляем запрос из браузера
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // -ОДИН ПАРАМЕТР
+        String param = req.getParameter("parame"); // ВСЕГДА получаем строку(то что придет после ? знака)
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter(); // поток в браузер
+        writer.write("<h1>" + param + "</h1>");
+    }
+
+
+}
